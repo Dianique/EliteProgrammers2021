24a8a90619091862a8ad564092d4faede07edc7d
Dianique <ashleydianique@yahoo.com>
2021-06-27 17:24:56 -0400

    Add changes

---
+++ src/Dianique.java
@@ -0,0 +1,19 @@
+public class Dianique extends EliteProgrammer {
+
+    @Override
+    public String getName() {
+        return "Dianique";
+    }
+
+    @Override
+    public String getCurrentFavoriteSong() {
+        return "Work";
+    }
+
+    @Override
+    public String getFavoriteFood() {
+        return "Indian Food";
+    }
+}
+
+
